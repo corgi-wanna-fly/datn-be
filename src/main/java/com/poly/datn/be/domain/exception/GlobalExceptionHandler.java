a1f5c5d39c22a74251c49816b87fe0d22000a81e
cvdai020401 <chudai010421@gmail.com>
2022-06-04 23:58:11 +0700

    Api get product by id

---
+++ src/main/java/com/poly/datn/be/domain/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.poly.datn.be.domain.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler({AppException.class})
+    public ResponseEntity<Object> handleAppException(AppException ex){
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("Timestamp", new Date());
+        body.put("Status", HttpStatus.BAD_REQUEST);
+        body.put("Errors", ex.showDetail());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
