c8707c0d14bf5677802a7becd3f733e92dfcb0ea
cvdai020401 <chudai010421@gmail.com>
2022-07-13 02:14:46 +0700

    crud voucher

--- src/main/java/com/poly/datn/be/api/VoucherApi.java
+++ src/main/java/com/poly/datn/be/api/VoucherApi.java
@@ -2,14 +2,16 @@ package com.poly.datn.be.api;
 
 import com.poly.datn.be.domain.constant.AppConst;
 import com.poly.datn.be.domain.constant.VoucherConst;
+import com.poly.datn.be.domain.exception.AppException;
+import com.poly.datn.be.entity.Voucher;
 import com.poly.datn.be.service.VoucherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -20,5 +22,21 @@ public class VoucherApi {
     public ResponseEntity getVoucherByCode(@RequestParam("code") String code){
         return new ResponseEntity(voucherService.getVoucherByCode(code), HttpStatus.OK);
     }
-    String a;
+    @PostMapping(VoucherConst.API_VOUCHER_CREATE)
+    public ResponseEntity<Voucher> save(@Valid @RequestBody Voucher vou){
+        if(voucherService.exitsByCode(vou.getCode())){
+            throw new AppException(VoucherConst.MSG_ERROR_CODE_DUPLICATE);
+        }
+        return new ResponseEntity<>(voucherService.saveVoucher(vou),HttpStatus.CREATED);
+    }
+    @PostMapping(VoucherConst.API_VOUCHER_UPDATE)
+    public ResponseEntity<Voucher> update(@Valid @RequestBody Voucher vou ,
+                                          @PathVariable("id") Optional<Long> id) throws AppException {
+        return new ResponseEntity<>(voucherService.update(vou, id.get()),HttpStatus.OK);
+    }
+    @GetMapping(VoucherConst.API_VOUCHER_REMOVE)
+    public ResponseEntity<String> remove(@PathVariable("id") Optional<Long> id) throws AppException{
+        voucherService.delete(id.orElse(null));
+        return new ResponseEntity<>("Succesfully!" , HttpStatus.OK);
+    }
 }
--- src/main/java/com/poly/datn/be/entity/Voucher.java
+++ src/main/java/com/poly/datn/be/entity/Voucher.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
 import java.time.LocalDate;
 import java.util.Collection;
 
@@ -23,12 +25,17 @@ public class Voucher {
     private String code;
     @Column(name = "create_date")
     private LocalDate createDate;
+    @Future
     @Column(name = "expire_date")
     private LocalDate expireDate;
     @Column(name = "discount")
     private Integer discount;
+
     @Column(name = "count")
+    @Min(1)
     private Integer count;
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
     @OneToMany(mappedBy = "voucher")
     @JsonIgnore
     private Collection<Order> orders;
