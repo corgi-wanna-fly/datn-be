7d143bd083e15d60924623f2e17bff237d8c4c6a
manhqnph11859 <62094866+manhqnph11859@users.noreply.github.com>
2022-07-14 22:39:30 +0700

    manh up

---
+++ src/main/java/com/poly/datn/be/domain/constant/CategoryConst.java
@@ -0,0 +1,13 @@
+package com.poly.datn.be.domain.constant;
+
+public class CategoryConst {
+    public static final String API_CATEGORY = "api/site/category";
+    public static final String FIND_ALL = "/findall";
+    public static final String CREATE = "/create";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete";
+    public static final String ADD_PRODUCT_CATEGORY = "/add_product_category";
+    public static final String FALSE = "FALSE";
+
+
+}
