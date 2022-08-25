2d7c3df37ed10f8d41bfc1d2bea4c78f44c8d0ad
Tan Vu <tanvx308@gmail.com>
2022-08-26 00:11:55 +0700

    tanvx fix

--- src/main/java/com/poly/datn/be/util/MailUtil.java
+++ src/main/java/com/poly/datn/be/util/MailUtil.java
@@ -51,7 +51,7 @@ public class MailUtil {
 
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("tanvxph13005@fpt.edu.vn", "SpringBoot@994");
+                return new PasswordAuthentication("", "");
             }
         });
         Message msg = new MimeMessage(session);
